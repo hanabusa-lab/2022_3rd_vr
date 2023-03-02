d1ede146971ef2b344036d8745999807956322ea
senaihara <ai_sen@mrh.biglobe.ne.jp>
2023-03-03 00:45:48 +0900

    update clicksniper

---
+++ clicksniper/Assets/Scripts/BallDelete.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class BallDelete : MonoBehaviour
+{
+    void Update()
+    {
+        //Y座標-10まで落下したとき
+        if(transform.position.y <= -10f)
+        {
+            //オブジェクトを削除する
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ clicksniper/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; //シーンをロードする際に必要
+ 
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] targetArray;
+    private int count;
+    private float time;
+    private int vecY;
+    public Text timeUp;
+    private float lastTime = 60.0f;
+    public GameObject retryButton;  //リトライボタンを入れる変数
+ 
+    void Start()
+    {
+        count = 0;
+        retryButton.SetActive(false); //リトライボタンを非表示
+    }
+ 
+    void Update()
+    {
+        if(count == targetArray.Length)
+        {
+            lastTime -= Time.deltaTime;
+            if(lastTime <= 0 )
+            {
+                timeUp.text = "TIME UP";
+                GetComponent<BallShot>().enabled = false;
+                retryButton.SetActive(true); //リトライボタンを表示
+            }
+        }
+        else 
+        {
+            time -= Time.deltaTime;
+            if(time <= 0.0f)
+            {
+                vecY = Random.Range(0,12);
+                //Instantiate(targetArray[count],new Vector3(-20, vecY, -2),Quaternion.identity);
+                time = 5.0f;
+                count++;
+            }
+        } 
+    }
+ 
+    //シーンを再ロードする（リトライ）
+    public void Retry()
+    { 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+　　}
+}
