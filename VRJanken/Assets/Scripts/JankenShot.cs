c9927c711508effc6f2d6692a51f45b547eece50
sen <ai_sen@mrh.biglobe.ne.jp>
2023-03-04 16:35:54 +0900

    name change to VRJanken

---
+++ VRJanken/Assets/Scripts/JankenShot.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class JankenShot : MonoBehaviour
+{
+    public GameObject[] prefab; //ボールのPrefabデータを入れる
+    public float power; //ボールに加える力
+    private float time; //連射防止
+    [SerializeField] GameObject player;
+    // private float ballSpeed = 10.0f;
+    private float timeTotal = 0f;
+ 
+    void Start()
+    {
+        time = 5.0f; //タイムを2秒で始めて、最初の1回は発射OK
+        timeTotal = 0f;
+    }
+ 
+    void Update()
+    {
+        time += Time.deltaTime;　//連射防止 カウントアップタイマー
+        timeTotal += Time.deltaTime;
+
+        // Transform originTransform = transform;
+        // transform.Translate(0, Random.Range(-3, 3), Random.Range(-3, 3), Space.World); // ワールドのX軸方向に1移動
+        transform.position = new Vector3(-20, Random.Range(5, 15), Random.Range(-15, 15));
+        transform.LookAt(player.transform);
+        float timeSpan = 3f;
+        if(timeTotal > 30f){
+            timeSpan = 1f;
+        }
+        if(time >= timeSpan)　//連射防止 2秒たてば実行
+        {
+            shot();
+            time = 0.0f;　//連射防止 タイマーを0に戻す
+            //}
+        }
+        // transform.position = originTransform.position;
+    }
+
+    void shot()
+    {
+        // マテリアル配列の添え字をランダムで設定（0～4）
+        int jankenNo = Random.Range(0, 3);
+        GameObject shotObj = Instantiate(prefab[jankenNo], transform.position, Quaternion.identity);
+        shotObj.GetComponent<Rigidbody>().velocity = transform.forward * power;
+        // Rigidbody shotRigidbody = shotObj.GetComponent<Rigidbody>();
+        // shotRigidbody.AddForce(transform.forward * power);
+    }
+}
---
+++ VRJanken/Assets/Scripts/ScoreCount.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //テキストを扱う場合に必要
+ 
+public class ScoreCount : MonoBehaviour
+{
+    public int score = 0; //スコアを入れる変数
+    public Text ScoreText; //表示するテキスト
+    public Material[] ColorSet;
+    private int jankenHand = 0;
+
+    [SerializeField]  AudioSource source1;
+    [SerializeField]  AudioSource source2;
+    [SerializeField]  AudioClip clip1;
+    [SerializeField]  AudioClip clip2;
+ 
+    void Update()
+    {
+        //scoreのデータをテキスト形式に変換。スコアテキストを表示する。
+        ScoreText.text = "SCORE: " + score.ToString();
+        if (Input.GetKey (KeyCode.Z)) {
+            jankenHand = 1;
+        }
+        if (Input.GetKey (KeyCode.X)) {
+            jankenHand = 2;
+        }
+        if (Input.GetKey (KeyCode.C)) {
+            jankenHand = 3;
+        }
+        if (Input.GetKey (KeyCode.V)) {
+            jankenHand = 0;
+        }
+        gameObject.GetComponent<Renderer>().material = ColorSet[jankenHand];
+
+    }
+ 
+    //オブジェクトがぶつかったときの処理
+    public void OnCollisionEnter(Collision collision)
+    {
+        //ぶつかった相手にScorePointタグがついているとき
+        if (collision.gameObject.CompareTag("JankenChoki"))
+        {
+            if(jankenHand == 1){
+                source1.PlayOneShot(clip1);
+                Destroy(collision.gameObject); //オブジェクトを消す
+                score++; //scoreを1増やす
+            }else{
+                source2.PlayOneShot(clip2);
+            }
+        }
+        if (collision.gameObject.CompareTag("JankenGu"))
+        {
+            if(jankenHand == 3){
+                source1.PlayOneShot(clip1);
+                Destroy(collision.gameObject); //オブジェクトを消す
+                score++; //scoreを1増やす
+            }else{
+                source2.PlayOneShot(clip2);
+            }
+        }
+        if (collision.gameObject.CompareTag("JankenPa"))
+        {
+            if(jankenHand == 2){
+                source1.PlayOneShot(clip1);
+                Destroy(collision.gameObject); //オブジェクトを消す
+                score++; //scoreを1増やす
+            }else{
+                source2.PlayOneShot(clip2);
+            }
+        }
+ 
+        //ぶつかった相手にDeleteタグがついているとき
+        if (collision.gameObject.CompareTag("Delete"))
+        {
+            Destroy(collision.gameObject); //オブジェクトを消す
+        }
+    }
+}
